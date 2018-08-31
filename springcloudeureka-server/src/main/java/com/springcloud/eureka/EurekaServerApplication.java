6dfde57d96ff7fd1215c46076e1c83038d9d42e1
Terry <235490059@qq.com>
2018-08-31 13:32:39 +0800

    spirngcloud

---
+++ springcloudeureka-server/src/main/java/com/springcloud/eureka/EurekaServerApplication.java
@@ -0,0 +1,11 @@
+package com.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
