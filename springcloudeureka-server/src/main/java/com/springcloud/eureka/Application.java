bc25a55d170ffa815d5ffc62d0e764c09ef9de69
Terry <235490059@qq.com>
2018-08-31 13:34:39 +0800

    spirngcloud

--- springcloudeureka-server/src/main/java/com/springcloud/eureka/Application.java
+++
@@ -1,9 +0,0 @@
-package com.springcloud.eureka;
-
-public class Application {
-
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
-    }
-
-}
